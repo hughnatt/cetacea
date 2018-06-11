3e916529b5fe12b727989a6121c0ff15fd14cc8e
Blow <biow@hotmail.fr>
2018-06-11 09:51:23 +0200

    2: Auto stash before merge of "dev-aquatic-2" and "master"

--- ricm3.game/game.whaler/edu/ricm3/game/whaler/GameMain.java
+++ ricm3.game/game.whaler/edu/ricm3/game/whaler/GameMain.java
@@ -26,7 +26,7 @@ import edu.ricm3.game.whaler.Game_exception.Tile_exception;
 
 public class GameMain {
 
-	public static void main(String[] args) throws Map_exception, Location_exception, Tile_exception {
+	public static void main(String[] args) throws Exception {
 
 		// construct the game elements: model, controller, and view.
 		Model model = new Model();
