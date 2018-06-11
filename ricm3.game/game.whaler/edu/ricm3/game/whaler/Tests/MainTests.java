5294fab3a0411e2b6107ed65f5f213d36f243175
Hugh Natt <hughnatt@protonmail.com>
2018-06-11 15:21:13 +0200

    Tested some tests

--- ricm3.game/game.whaler/edu/ricm3/game/whaler/Tests/MainTests.java
+++ ricm3.game/game.whaler/edu/ricm3/game/whaler/Tests/MainTests.java
@@ -2,13 +2,40 @@ package edu.ricm3.game.whaler.Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStream;
+
 import org.junit.jupiter.api.Test;
+
+import edu.ricm3.game.parser.*;
+import edu.ricm3.game.parser.Ast.AI_Definitions;
+import edu.ricm3.game.whaler.*;
+import edu.ricm3.game.whaler.Entities.*;
+import edu.ricm3.game.whaler.Interpretor.*;
 
 class MainTests {
 
 	@Test
-	void test() {
-		//fail("Not yet implemented");
+	void test() throws Exception {
+		long start = System.currentTimeMillis();
+		
+		
+		
+		String input = "Mover(Init){ * (Init): True ? Move(E) :(Init) }";
+		
+		AutomataParser.from_string(input);
+		
+		IAutomata[] autos =  ((AI_Definitions) ast).make();
+		
+		
+		Player p = new Player(new Location(10,10), null, null, m, Direction.EAST, autos[0]);
+		
+		p.step(System.currentTimeMillis() - start);
+		
+		assert(p.getx() == 11);
+		assert(p.gety() == 10);
+			
 	}
 
 }
