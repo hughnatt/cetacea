b84a9f01b492d624747b8306c252e0a64086c62f
reymones <estelle-r@hotmail.fr>
2018-06-12 17:55:16 +0200

    Selected Automata menu almost ok

--- ricm3.game/game.framework/edu/ricm3/game/AutomataSelection.java
+++ ricm3.game/game.framework/edu/ricm3/game/AutomataSelection.java
@@ -28,18 +28,17 @@ import edu.ricm3.game.whaler.Interpretor.IAutomata;
 
 public class AutomataSelection {
 
-	
 	JFrame m_select;
 	JFrame frame;
 	JLabel m_title;
 	JPanel m_ctr;
 	GameUI m_g;
 	JComboBox<?> b[];
-	
+
 	public AutomataSelection(GameUI g) {
-		m_g =g;
+		m_g = g;
 	}
-	
+
 	public void create_frame() {
 		m_select = new JFrame();
 		m_select.setTitle("Cetacea - Automata Selection");
@@ -70,13 +69,13 @@ public class AutomataSelection {
 
 		m_select.setVisible(true);
 	}
-	
+
 	public void create_automata_selection() {
 
 		// Creation des bouttons
 		CreateButton breturn = new CreateButton("RETURN", "game.whaler/sprites/arrow_return.png",
 				"game.whaler/sprites/arrow_return.png");
-		
+
 		CreateButton bvalider = new CreateButton("VALIDER", "game.whaler/sprites/check.png",
 				"game.whaler/sprites/check_over.png");
 
@@ -91,72 +90,60 @@ public class AutomataSelection {
 		JPanel return_button = new JPanel();
 		return_button.setOpaque(false);
 		return_button.add(breturn);
-		
+
 		JPanel valide_button = new JPanel();
 		valide_button.setOpaque(false);
 		valide_button.add(bvalider);
-		
+
 		JPanel b1 = new JPanel();
 		b1.setLayout(new BoxLayout(b1, BoxLayout.LINE_AXIS));
 		b1.add(return_button);
 		b1.add(valide_button);
 
-
-		//Création tableau déroulant avec les bons automates
+		// Création tableau déroulant avec les bons automates
 		frame = new JFrame();
-		GridLayout grid = new GridLayout(2,6);
+		GridLayout grid = new GridLayout(6, 2);
 		frame.setLayout(grid);
 		grid.setHgap(20);
-		
+		grid.setVgap(20);
+
 		JPanel b2 = new JPanel();
 		b2.setLayout(grid);
-	
-		BufferedImage image;
-		try {
-			image = ImageIO.read(new File("game.whaler/sprites/cetacea.png"));
-			 JLabel label = new JLabel(new ImageIcon(image));
-			 b2.add(label);
-			 image = ImageIO.read(new File("game.whaler/sprites/Destroyer_menu.png"));
-			 label = new JLabel(new ImageIcon(image));
-			 b2.add(label);
-			 image = ImageIO.read(new File("game.whaler/sprites/Destroyer_menu.png"));
-			 label = new JLabel(new ImageIcon(image));
-			 b2.add(label);
-			 image = ImageIO.read(new File("game.whaler/sprites/Destroyer_menu.png"));
-			 label = new JLabel(new ImageIcon(image));
-			 b2.add(label);
-			 image = ImageIO.read(new File("game.whaler/sprites/Destroyer_menu.png"));
-			 label = new JLabel(new ImageIcon(image));
-			 b2.add(label);
-			 image = ImageIO.read(new File("game.whaler/sprites/Destroyer_menu.png"));
-			 label = new JLabel(new ImageIcon(image));
-			 b2.add(label);
-			 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 
 		Model m_model = (Model) m_g.m_model;
 		IAutomata[] items = m_model.automata_array;
+
+		BufferedImage[] image = new BufferedImage[6];
+		try {
+			image[0] = ImageIO.read(new File("game.whaler/sprites/cetacea.png"));
+			image[1] = ImageIO.read(new File("game.whaler/sprites/cetacea.png"));
+			image[2] = ImageIO.read(new File("game.whaler/sprites/cetacea.png"));
+			image[3] = ImageIO.read(new File("game.whaler/sprites/cetacea.png"));
+			image[4] = ImageIO.read(new File("game.whaler/sprites/cetacea.png"));
+			image[5] = ImageIO.read(new File("game.whaler/sprites/cetacea.png"));
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		b = new JComboBox[6];
 		int i = 0;
 		while (i < 6) {
-			//We create 6 ComboBox, one for each entity, and add it to the panel
+			JLabel label = new JLabel(new ImageIcon(image[i]));
+			b2.add(label);
+			// We create 6 ComboBox, one for each entity, and add it to the panel
 			b[i] = new JComboBox<Object>(items);
-			b2.setSize(new Dimension(75,25));
+			b2.setSize(new Dimension(50, 50));
 			b2.add(b[i]);
-			
 			i++;
 		}
 
-		
-		m_select.add(b1, BorderLayout.SOUTH); //bouton retour
-		m_select.add(b2, BorderLayout.CENTER );
+		m_select.add(b1, BorderLayout.SOUTH); // bouton retour
+		m_select.add(b2, BorderLayout.CENTER);
 		m_select.setVisible(true);
 	}
-	
+
 	public void validate_automata() {
 		File file = new File("game.whaler/sprites/choix_automates.txt");
 		BufferedWriter out = null;
@@ -175,11 +162,11 @@ public class AutomataSelection {
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-}
+		}
 	}
 
 	public class IsClicked implements ActionListener {
-		//On verifie où on clique
+		// On verifie où on clique
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String event = e.getActionCommand();
