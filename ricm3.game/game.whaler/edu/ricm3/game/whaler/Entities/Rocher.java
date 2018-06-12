8f07cbe53f62a9b090885c25a2e65b9e7071c7ce
Blow <biow@hotmail.fr>
2018-06-12 12:57:59 +0200

    Stone added

---
+++ ricm3.game/game.whaler/edu/ricm3/game/whaler/Entities/Rocher.java
@@ -0,0 +1,30 @@
+package edu.ricm3.game.whaler.Entities;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import edu.ricm3.game.whaler.Location;
+import edu.ricm3.game.whaler.Model;
+import edu.ricm3.game.whaler.Game_exception.Automata_Exception;
+import edu.ricm3.game.whaler.Game_exception.Game_exception;
+
+public class Rocher extends Static_Entity{
+	
+	public Rocher(Location pos, BufferedImage sprite, BufferedImage underSprite, Model model) throws Game_exception {
+		super(pos, true, sprite, underSprite, model);
+	}
+
+	@Override
+	public void step(long now) throws Game_exception, Automata_Exception {
+	}
+
+	@Override
+	public void paint(Graphics g, Location map_ref) {
+		g.drawImage(m_sprite, (this.getx() - map_ref.x) * 32, (this.gety() - map_ref.y) * 32, 32, 32, null);
+	}
+
+	@Override
+	public void paint_under(Graphics g, Location map_ref) {
+		
+	}
+}
