1ead06914fc1e18f4aeeda9fff18767dcb1d3502
Blow <biow@hotmail.fr>
2018-06-13 13:15:54 +0200

    getter

--- ricm3.game/game.whaler/edu/ricm3/game/whaler/Tile.java
+++ ricm3.game/game.whaler/edu/ricm3/game/whaler/Tile.java
@@ -11,6 +11,10 @@ import edu.ricm3.game.whaler.Game_exception.Tile_exception;
 public class Tile {
 	LinkedList<Entity> m_level;
 
+	public LinkedList<Entity> getList() {
+		return m_level;
+	}
+
 	/*
 	 * List Representation of multiple entities superposed
 	 */
