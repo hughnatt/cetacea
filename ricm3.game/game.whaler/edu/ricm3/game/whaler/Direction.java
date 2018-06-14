426aa45b962a87178c9630a6bfa308989bb7bad5
Diss <m.dumax-vorzet@orange.fr>
2018-06-14 15:15:55 +0200

    Ajout de méthode sur Direction et application

--- ricm3.game/game.whaler/edu/ricm3/game/whaler/Direction.java
+++ ricm3.game/game.whaler/edu/ricm3/game/whaler/Direction.java
@@ -4,5 +4,37 @@ package edu.ricm3.game.whaler;
  * Cardinal Directions and Relative Position
  */
 public enum Direction {
-	NORTH, SOUTH, WEST, EAST, FORWARD, BACKWARD, RIGHT, LEFT
+	NORTH, SOUTH, WEST, EAST, FORWARD, BACKWARD, RIGHT, LEFT;
+
+	public Direction RotationToRight() {
+		switch (this) {
+		case NORTH:
+			return EAST;
+		case EAST:
+			return SOUTH;
+		case SOUTH:
+			return WEST;
+		case WEST:
+			return NORTH;
+		default:
+			System.out.println("Unknown Direction, will be interpreted as NORTH");
+			return Direction.NORTH;
+		}
+	}
+
+	public Direction RotationToLeft() {
+		switch (this) {
+		case NORTH:
+			return WEST;
+		case EAST:
+			return NORTH;
+		case SOUTH:
+			return EAST;
+		case WEST:
+			return SOUTH;
+		default:
+			System.out.println("Unknown Direction, will be interpreted as NORTH");
+			return Direction.NORTH;
+		}
+	}
 }
